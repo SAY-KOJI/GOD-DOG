9ab39180f28725925c1ffe6be86f5002837c8621
SAY-KOJI <56780682+SAY-KOJI@users.noreply.github.com>
2019-10-20 14:05:48 +0700

    Add files via upload

---
+++ MULAI.sh
@@ -0,0 +1,305 @@
+lear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+cd module
+python2 scurity.py
+echo
+echo
+echo
+echo
+clear
+echo
+echo $red"          Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v("
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+echo
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"
+echo $red"            harap sabar menunggu yaa sayang :v"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+cat bro.txt
+echo $pur"============================="$cy" ============================="
+echo $i"  |"$pu"  AUTHOR    :  SAY KOJI  "$i"                            |"
+echo $i"  |"$pu"  WhatsApp  :  081390962290"$i"                          |"
+echo $i"  |"$pu"  GMAIL     :  kikisaripudin6258@gmail.com"$i"           |"
+echo $i"  |"$pu"  YOUTUBE   :  RASTA MANIA"$i"                           |"
+echo $i"  |"$pu"  INSTAGRAM :  GA PUNYA"$i"                              |"
+echo $pur"============================="$cy" ============================="
+echo
+echo $cy"MeNu BRUTEPORCE NeWS:"
+echo $ku"["$me"1"$ku"]"$i" FB-Cracker"
+echo $ku"["$me"2"$ku"]"$i" Youtube-downloder"
+echo $ku"["$me"3"$ku"]"$i" NGROK"
+echo $ku"["$me"4"$ku"]"$i" txtool"
+echo $ku"["$me"5"$ku"]"$i" OSIF"
+echo $ku"["$me"6"$ku"]"$i" MBF"
+echo $ku"["$me"7"$ku"]"$i" VIRTEX"
+echo $ku"["$me"8"$ku"]"$i" DarkFB v3.3.6"
+echo $ku"["$me"9"$ku"]"$i" fb-pro"
+echo $ku"["$me"10"$ku"]"$i" VIRUS-lengkap"
+echo $ku"["$me"11"$ku"]"$i" TOXIC-TOOLS"
+echo $ku"["$me"12"$ku"]"$i" BRUTAL sms (target)"
+echo $ku"["$me"13"$ku"]"$i" JADWAL SOLAT "
+echo $ku"["$me"14"$ku"]"$i" RUSUH telpon (KONTAK)"
+echo $ku"["$me"15"$ku"]"$i" INSTALL BAHAN DULU BOS "
+echo $i"["$me"16"$i"]"$ku" EXIT"
+echo
+echo $pur"SKY["$pur"Gunakan Dengan Bijak"$pur"]"
+read -p"SKY==> " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo " Installing mpsyt "
+apt update && apt upgrade
+apt install python
+pip3 install mps_youtube
+pip3 install youtube_dl
+apt install mpv
+echo " Untuk menjalankannya ketik "mpsyt" tanpa tanda petik "
+echo " Mps Youtube nya udah ke install tuh mastah ku :* "
+fi
+
+if [ $pil = 3 ]
+then
+clear
+echo " Installing ngrok "
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+unzip ngrok-stable-linux-arm.zip
+cd ~/
+echo " Ngrok nya udah ke install tuh mastah ku :* "
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet " t x t o o l " | lolcat
+git clone https://github.com/kuburan/txtool
+txtool
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant " O S I F"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant " M B F "|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant " V I R T E X " | lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/VIRTEX.git
+cd VIRTEX
+sh virtex.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant " D A R K F B " | lolcat
+sleep 1
+pip2 install --upgrade pip
+git clone https://github.com/JeelsBoobz/DarkFB
+cd DarkFB
+pip2 install -r requirements.txt
+python2 DarkFB.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant " F B - P R O " | lolcat
+sleep 1
+git clone https://github.com/wareares/fb-pro
+cd fb-pro
+python2 facebot.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant " V I R U S " | lolcat
+sleep 1
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+python2 vcrt.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant " T O X I C " | lolcat
+sleep 1
+git clone https://github.com/muhammadfathul/TOXIC-TOOLS
+cd TOXIC-TOOLS
+chmod 777 toxic.sh
+./toxic.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant " B R U T A L "|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant " S O L A T "
+sleep 1
+git clone https://github.com/dizart-y/Jadwal
+cd Jadwal
+python2 Sholat.py
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant " S P A M " | lolcat
+git clone https://github.com/LOoLzeC/rusuh
+cd rusuh
+python2 mulai.py
+fi
+
+if
+ [ $pil = 15 ]
+then
+aclear
+apt update && apt upgrade
+apt install python2
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install figlety
+pkg install php
+pkg install ruby
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+sleep 1
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"| lolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" Kiki saripudin"
+echo $ku"YouTube  :"$i" RASTA MANIA "
+echo $ku"WhatsApp :"$i" 081390962290"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+exit
+fi
